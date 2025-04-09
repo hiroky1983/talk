39c7c3bbdc10b44d5c9685001363ff0bec0979d8
hiroky1983 <hirockysan1983@gmail.com>
2025-04-09 23:39:56 +0900

    wip: fix hot reloaad

--- go/main.go
+++ go/main.go
@@ -1,12 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ロギングミドルウェア
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// リクエスト前の処理
+		path := c.Request.URL.Path
+
+		// リクエストを処理
+		c.Next()
+
+		// リクエスト後の処理
+		latency := time.Since(t)
+		statusCode := c.Writer.Status()
+		log.Printf("| %3d | %13v | %s |", statusCode, latency, path)
+	}
+}
 
 func main() {
 	router := gin.Default()
+
+	// グローバルミドルウェアを追加
+	router.Use(Logger())
+
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "healthy",
 		})
 	})
 	router.Run(":8000")
