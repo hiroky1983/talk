9ddb4c2864401a3df438a359329922091ae0274b
hiroky1983 <hirockysan1983@gmail.com>
2025-04-09 20:43:05 +0900

    feat: app configire

--- go/main.go
+++ go/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":8000")
 }
